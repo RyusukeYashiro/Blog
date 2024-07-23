d418da9c31371f5d897c1f53a16762240ace7d73
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-07-23 22:07:00 +0900

    add css

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './style/Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className='postContents'>
+        <div className='postHeader'>
+          <h1>タイトル</h1>
+        </div>
+        <div className='postContainer'>
+          僕はもうあのさそりのようにほんとうにみんなの幸（さいわい）のためならば僕のからだなんか百ぺんやいてもかまわない。
+        </div>
+        <div className='Deletebutton'>
+          <h3>yashiro</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
