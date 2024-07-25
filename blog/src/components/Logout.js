2e6a3699c6123629d6d052e7aee50d9b6ce2bfb0
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-07-25 14:02:01 +0900

    logoutの機能のデザインとコメントアウトを追加

--- blog/src/components/Logout.js
+++ blog/src/components/Logout.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import "./style/Login.css";
-import { signInWithPopup, signOut } from 'firebase/auth';
-import { auth, provider } from '../fireBase';
+import "./style/Logout.css";
+import { signOut } from 'firebase/auth';
+import { auth } from '../fireBase';
 import { useNavigate } from "react-router-dom";
 
+// 引数で受け取るのはログイン情報を保持しているか否かの変数
 const Logout = ({ setIsAuth }) => {
   const navigate = useNavigate();
   const logout = () => {
       signOut(auth).then(() => {
+        // ログイン情報を保持しているlocalStorageをクリア
         localStorage.clear();
+        // ここでfalseにしてログインしていない状態にする
         setIsAuth(false);
+        // ログインと同じくホームへリダイレクトを飛ばす
         navigate('/login');
       });
   };
 
-  return (
-   <div class="logout-page">
-    <h1 class="">Logout</h1>
-    <div>!ログインではgoogleを使います!</div>
-    <button onClick={logout}>ログアウト</button>
-   </div>
+  return(
+    <div className="logout-page">
+    <div className='logoutContainer'>
+      <h1>Logout</h1>
+      <div className='logoutDescription'>!現在のアカウントからログアウトします!</div>
+      <button onClick={logout}>Logout</button>
+    </div>
+    </div>
   );
-}
+};
 
 export default Logout;
